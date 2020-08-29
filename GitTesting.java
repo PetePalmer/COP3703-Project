9ebcdd463f099352f2788c9ef1723cc4c15d352f
Nicholas Myers <65913043+nickmyers998@users.noreply.github.com>
2020-08-29 14:15:02 -0400

    Create GitTesting.java
    Testing

---
+++ GitTesting.java
@@ -0,0 +1,13 @@
+//Hello everyone, it would be great if everyone could write a short print statement here to show that you can get into the repo can submit code, thanks!
+
+public class GitTesting {
+
+    public static void main(String[] args){
+    
+        System.out.println("Hey it's Nick");
+        
+        
+        
+        
+    }
+}
